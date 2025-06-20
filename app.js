13b85ed013a9d32fe9f0aca41ec58204010a4246
EduardoRo1 <151682254+EduardoRo1@users.noreply.github.com>
2025-06-19 19:45:39 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 import mongoose from "mongoose";
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+//corse
+import cors from "cors";
+//palabras y falta .js
+import { test } from "./backend/controllers/palabras.controller.js";
 
-
-mongoose.connect (process.env.urlbd)
+//flataban los parentesis
+dotenv.config();
+//connect en vez de conect
+mongoose.connect(process.env.urlbd)
 .then(()=>{
-    console.log("Si jalo")
+    console.log("funciona la conexión de la base")
 })
-.catch((error)=>{
-    console.log("No jalo", error)
+.catch(()=>{
+    console.log("no funciona la conexión a la base de datos")
 })
 
 const app = express();
-app.use(cors());
+//use en vez de usa y cors en vez de corse
+app.use(cors);
+//listen
 app.listen(4000, ()=>{
+    //log en vez de long
     console.log("Escuchando al servidor")
 })
+//le faltaban los "()" y el ";"
+test();
